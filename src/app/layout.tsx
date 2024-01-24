af4625bd2a99ee43a1360948ce6bc21790816a54
vishalskillprompt <vishalthapa@skillprompt.com>
2024-01-24 21:09:44 +0545

    fix bug #4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,22 +3,24 @@
 import { Quicksand } from "next/font/google";
 import "./style/globals.css";
 import NavigationBar from "@/components/navbar";
+import Head from "next/head";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
-      <div className="bg-gradient-to-b from-light fixed h-[100vh] w-full">
-        <div className={quicksand.className}>
-          <NavigationBar />
+      <Head>
+        <div>Portfolio</div>
+      </Head>
+      <body>
+        <div className="bg-gradient-to-b from-light fixed h-[100vh] w-full">
+          <div className={quicksand.className}>
+            <NavigationBar />
+          </div>
+          <div className={quicksand.className}>{children}</div>
         </div>
-        <div className={quicksand.className}>{children}</div>
-      </div>
+      </body>
     </html>
   );
 }
