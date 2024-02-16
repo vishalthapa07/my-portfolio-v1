f722b21a7bcb5965c99b3722bbc93f8afa26b110
vishalskillprompt <vishalthapa@skillprompt.com>
2024-02-16 15:22:57 +0545

    remove unused library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,6 @@ import Introduction from "@/components/introduction";
 export default function Home() {
   return (
     <>
-      {/* <div className="grid grid-cols-2 bg-gradient-to-b from-light w-full h-full">
-        <div className="w-full flex items-center justify-center">
-          <Introduction />
-        </div>
-        <div className="w-full flex items-center justify-center">
-          <img src="/developer01.gif" alt="developer" />
-        </div>
-      </div> */}
       <div className="bg-primary h-screen px-36 flex items-center">
         <Introduction />
       </div>
