45495e4346d736411cd66a84bc62b374b2c84bde
vishalskillprompt <vishalthapa@skillprompt.com>
2024-03-06 22:31:22 +0545

    implement 3d card for introduction section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        primary: "#7F27FF",
+        primary: "#616161",
         white: "#FFFFFF",
       },
     },
